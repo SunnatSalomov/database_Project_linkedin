e320f40d1acf00cd8db34afd515f36602f7eebfa
Sunnat Salomov <sunish2412@gmail.com>
2023-08-01 09:52:19 +0500

    create table

--- SqlCode.sql
+++ SqlCode.sql
@@ -1,1 +1,8 @@
---  shotta kod yoziladi
+create  table users(
+    id int serial primary key,
+    first_name varchar,
+    last_name varchar,
+    email varchar,
+    password varchar,
+    join_date date
+);
