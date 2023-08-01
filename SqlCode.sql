7f9664d86d681ac4056a21ded6549f97f2ef010a
bpard <bpardaboyev2000@gmail.com>
2023-08-01 10:20:38 +0500

    commit one from baxodir1

--- SqlCode.sql
+++ SqlCode.sql
@@ -1,8 +1,24 @@
-create  table users(
-    id int serial primary key,
+create table users
+(
+    id         serial primary key,
     first_name varchar,
-    last_name varchar,
-    email varchar,
-    password varchar,
-    join_date date
+    last_name  varchar,
+    email      varchar,
+    password   varchar,
+    join_date  date
 );
+create table post
+(
+    id        serial primary key,
+    user_id   int references users (id),
+    content   varchar,
+    post_date date
+);
+create table comment
+(
+    id           serial primary key,
+    user_id      int references users (id),
+    post_id      int references post (id),
+    content      character varying,
+    comment_date date default now()
+)
